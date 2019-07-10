18fb6586be41c4fa61e3b5143b15ac5bfb083707
James Hartill <jameshartill@gmail.com>
2019-07-10 12:36:10 +0200

    updates dependencies and production build

---
+++ build/precache-manifest.7c0d9a3aca2b1b50a3af706912ad3f7a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c01847326e0c6e46ea25685dbf4de6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "215de8945d82469bc322",
+    "url": "/static/css/main.54f1008b.chunk.css"
+  },
+  {
+    "revision": "9b64b1e9cae39cc382ad",
+    "url": "/static/js/2.948dcce4.chunk.js"
+  },
+  {
+    "revision": "215de8945d82469bc322",
+    "url": "/static/js/main.08f1e3ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab195afb23e6d3580f3a47709574747c",
+    "url": "/static/media/donautal.ab195afb.jpg"
+  },
+  {
+    "revision": "83ac2decdec9102d93e49fd2e223be8e",
+    "url": "/static/media/french-alps-09.83ac2dec.jpg"
+  },
+  {
+    "revision": "44cf5e7825f9b8d0152e10dd128f8cb7",
+    "url": "/static/media/hf4-monopile-feature.44cf5e78.jpg"
+  },
+  {
+    "revision": "c2582891720cbfe3b16232750c144bd2",
+    "url": "/static/media/hf4-turbine-feature.c2582891.jpg"
+  },
+  {
+    "revision": "b2183d349fcb3bb8f7169db39b47541d",
+    "url": "/static/media/html-5.b2183d34.png"
+  },
+  {
+    "revision": "07686004210dff0674c7a182234ecb24",
+    "url": "/static/media/jfms-feature.07686004.jpg"
+  },
+  {
+    "revision": "0ed5d817b21873cc22e1de55a892cbfd",
+    "url": "/static/media/leben-feature.0ed5d817.jpg"
+  },
+  {
+    "revision": "291b654158e943d06da1a72a54e673b6",
+    "url": "/static/media/leben-in-deutschland-test-iconx300.291b6541.png"
+  },
+  {
+    "revision": "55af14647c626a97a3ed33885d442ceb",
+    "url": "/static/media/leben-in-deutschland.55af1464.jpg"
+  },
+  {
+    "revision": "881b16102c2f2c4a49ada7ecf752b2bc",
+    "url": "/static/media/leben_app_iphone.881b1610.png"
+  },
+  {
+    "revision": "f0e746ec9d668cbf7f6d8e8015dc210d",
+    "url": "/static/media/mtindo-feature.f0e746ec.jpg"
+  },
+  {
+    "revision": "d2606e6b9b5732cd84a350cfe8f9542b",
+    "url": "/static/media/open-school-feature.d2606e6b.png"
+  },
+  {
+    "revision": "983671aed2127213823b95d5ef12babc",
+    "url": "/static/media/porthtowan4.983671ae.jpg"
+  },
+  {
+    "revision": "93a60387cf6adf2010a6014c7e0ed36d",
+    "url": "/static/media/react-logo-300.93a60387.png"
+  },
+  {
+    "revision": "2f6547473c2639ca2bf1ef997e8702b3",
+    "url": "/static/media/tiger-x-feature.2f654747.jpg"
+  }
+]);
